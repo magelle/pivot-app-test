49a0a0030026dc5ac31d9a5639227ae1cbfb7e08
Maxime GELLE <maxime.gelle@gmail.com>
2025-10-30 10:19:23 +0100

    Add id to parse pathParams

--- src/main/kotlin/pivot/app/test/CounterController.kt
+++ src/main/kotlin/pivot/app/test/CounterController.kt
@@ -1,9 +1,7 @@
 package pivot.app.test
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/")
@@ -11,22 +9,31 @@ class CounterController(
     private val counterRepo: CounterRepository
 ) {
 
-    @GetMapping("/counter")
-    fun getCounter() = (this.counterRepo.findCounterById(1) ?: Counter(0)).value()
+    @PutMapping("/counter/create")
+    fun getCounter(): Int {
+        val counter = Counter(0)
+        val id = LocalDateTime.now().toEpochSecond(java.time.ZoneOffset.UTC).toInt()
+        this.counterRepo.save(id, counter)
+        return id
+    }
 
-    @PostMapping("/counter/inc")
-    fun incCounter(): Int {
-        val counter = this.counterRepo.findCounterById(1) ?: Counter(0)
+    @GetMapping("/counter/{id}")
+    fun getCounter(@PathVariable id: Int): Int? =
+        this.counterRepo.findCounterById(id)?.value()
+
+    @PostMapping("/counter/{id}/inc")
+    fun incCounter(@PathVariable id: Int): Int {
+        val counter = this.counterRepo.findCounterById(id) ?: Counter(0)
         counter.inc()
-        this.counterRepo.save(1, counter)
+        this.counterRepo.save(id, counter)
         return counter.value()
     }
 
-    @PostMapping("/counter/dec")
-    fun decCounter(): Int {
-        val counter = this.counterRepo.findCounterById(1) ?: Counter(0)
+    @PostMapping("/counter/{id}/dec")
+    fun decCounter(@PathVariable id: Int): Int {
+        val counter = this.counterRepo.findCounterById(id) ?: Counter(0)
         counter.dec()
-        this.counterRepo.save(1, counter)
+        this.counterRepo.save(id, counter)
         return counter.value()
     }
 
